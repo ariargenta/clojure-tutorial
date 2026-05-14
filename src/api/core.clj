524560935a50b73ddc4a362456e9dbd5e3cc53af
ariargenta <55609849+ariargenta@users.noreply.github.com>
2026-05-14 15:08:24 -0600

    feat: add pedestal libraries

--- src/api/core.clj
+++ src/api/core.clj
@@ -1,7 +1,31 @@
 (ns api.core
-  (:require [api.config :as config]))
+  (:require [api.config :as config]
+            [io.pedestal.http :as http]
+            [io.pedestal.http.route :as route]))
+
+(defn respond-hello
+  [request]
+  {:status 200
+   :body "Hello, world!"})
+
+(def routes
+  (route/expand-routes
+    #{["/greet" :get respond-hello :route-name :greet]}))
+
+(defn create-server
+  [config]
+  (http/create-server
+    {::http/routes routes
+     ::http/type :jetty
+     ::http/join? false
+     ::http/port (-> config :server :port)}))
+
+(defn start
+  [config]
+  (http/start (create-server config)))
 
 (defn -main
   []
   (let [config (config/read-config)]
-  (println "Starting Clojure API service with configuration" config)))
+  (println "Starting Clojure API service with configuration" config)
+  (start config)))
