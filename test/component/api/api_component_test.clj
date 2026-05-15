3482c2f548cd3db6ca27a45f3577928192da8b55
ariargenta <55609849+ariargenta@users.noreply.github.com>
2026-05-14 21:44:33 -0600

    test: add simple tests

---
+++ test/component/api/api_component_test.clj
@@ -0,0 +1,5 @@
+(ns component.api.api-component-test
+  (:require [clojure.test :refer :all]))
+
+(deftest simple-api-test
+  (is (= 1 1)))
---
+++ test/unit/api/api_unit_test.clj
@@ -0,0 +1,5 @@
+(ns unit.api.api-unit-test
+  (:require [clojure.test :refer :all]))
+
+(deftest simple-passing-test
+  (is (= 1 1)))
