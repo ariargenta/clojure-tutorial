b331e8978efbea597d3b3a4515ffa5858a33693e
ariargenta <55609849+ariargenta@users.noreply.github.com>
2026-05-14 21:09:03 -0600

    refactor: move pedestal logic to component

--- src/api/core.clj
+++ src/api/core.clj
@@ -1,35 +1,16 @@
 (ns api.core
   (:require [api.config :as config]
-            [io.pedestal.http :as http]
-            [io.pedestal.http.route :as route]
             [com.stuartsierra.component :as component]
-            [api.components.testing :as test-component]))
-
-(defn respond-hello
-  [request]
-  {:status 200
-   :body "Hello, world!"})
-
-(def routes
-  (route/expand-routes
-    #{["/greet" :get respond-hello :route-name :greet]}))
-
-(defn create-server
-  [config]
-  (http/create-server
-    {::http/routes routes
-     ::http/type :jetty
-     ::http/join? false
-     ::http/port (-> config :server :port)}))
-
-(defn start
-  [config]
-  (http/start (create-server config)))
+            [api.components.testing :as test-component]
+            [api.components.pedestal-component :as pedestal-component]))
 
 (defn api-system
   [config]
   (component/system-map
-    :test-component (test-component/new-test-component config)))
+    :test-component (test-component/new-test-component config)
+    :pedestal-component
+    (component/using (pedestal-component/new-pedestal-component config)
+                     [:test-component])))
 
 (defn -main
   []
